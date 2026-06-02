f15a00419b16ec7f582a048b855384880ad5d0ca
Ayush <86874794+aayushc22@users.noreply.github.com>
2026-06-03 00:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (81.86%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,40 +1,39 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind=-1;
-        int n=nums.length;
-
+        int n = nums.length;
+        int ind = -1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                ind=i;
+                ind = i;
                 break;
             }
         }
-        if(ind==-1){
-            rev(nums,0,n-1);
-            return;
+        if(ind == -1){
+            reverse(0,n-1,nums);
+            
         }
-
+        else{
         for(int i=n-1;i>ind;i--){
             if(nums[i]>nums[ind]){
-                swp(nums,i,ind);
-                break;
-            }
+            swap(ind,i,nums);
+            break;}
         }
-        rev(nums,ind+1,n-1);
-        
+
+        reverse(ind+1,n-1,nums);}
+
     }
-     public void rev(int[] arr,int i,int j){
-        while(i<j){
-            int t=arr[i];
-            arr[i]=arr[j];
-            arr[j]=t;
-            i++;
-            j--;
-        }
+    public void reverse(int i, int j,int arr[]){
+
+        while(j>i){
+        int a = arr[i];
+        arr[i]=arr[j];
+        arr[j]=a;
+        i++;j--;
+   }
     }
-    public void swp(int[] arr,int i,int j){
-            int t=arr[i];
-            arr[i]=arr[j];
-            arr[j]=t;
+    public void swap(int i,int j, int arr[]){
+        int a =arr[i];
+        arr[i]=arr[j];
+        arr[j]=a;
     }
 }
